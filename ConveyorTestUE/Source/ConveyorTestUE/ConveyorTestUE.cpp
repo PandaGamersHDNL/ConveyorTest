ab426bcc029c77fb60ebbdfe89e43944062dc251
panda jacobs <jokke.jacobs@gmail.com>
2020-12-23 03:19:51 +0100

    creations project in UE4 -move gitignore in the project folder

---
+++ ConveyorTestUE/Source/ConveyorTestUE/ConveyorTestUE.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ConveyorTestUE.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ConveyorTestUE, "ConveyorTestUE" );
---
+++ ConveyorTestUE/Source/ConveyorTestUE/ConveyorTestUEGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ConveyorTestUEGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CONVEYORTESTUE_API AConveyorTestUEGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
